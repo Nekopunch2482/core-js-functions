884c5868a4c594ca9621cf27950942496db3aff3
Nekopunch <Nekopunch2482@yandex.ru>
2025-03-21 12:57:08 +0300

    feat: implement logger

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -169,8 +169,18 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argsString = args.map((a) => JSON.stringify(a)).join(',');
+
+    const logString = `${func.name}(${argsString})`;
+
+    logFunc(`${logString} starts`);
+    const result = func(...args);
+    logFunc(`${logString} ends`);
+
+    return result;
+  };
 }
 
 /**
